04723a79a547d38f49538db61bab1dcf95936ccf
Pablo <pablo.castro@witi.cl>
2019-08-29 12:43:55 -0400

    [FIX]: Se agrega modulo de add faltante

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { AddModule } from './add/add.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,7 +7,7 @@ import { DivideModule } from './divide/divide.module';
 import { MultiplyModule } from './multiply/multiply.module';
 
 @Module({
-  imports: [SubstractModule, DivideModule, MultiplyModule],
+  imports: [AddModule, SubstractModule, DivideModule, MultiplyModule],
   controllers: [AppController],
   providers: [AppService],
 })
